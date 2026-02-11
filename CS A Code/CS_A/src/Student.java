c4d4a9a518e371450c6ba9675c473bddceb0ee05
TeachToTech-in <posttoteachtotech@gmail.com>
2026-02-11 21:03:17 +0530

    CS Notes

---
+++ CS A Code/CS_A/src/Student.java
@@ -0,0 +1,18 @@
+
+public class Student {
+	private int rollno;
+	private String name;
+	private long contact;
+	
+	public void setValues(int r, String s, long c) {
+		rollno = r;
+		name = s;
+		contact= c;
+	}
+	public void display() {
+		System.out.println(rollno);
+		System.out.println(name);
+		System.out.println(contact);
+	}
+
+}
---
+++ CS A Code/CS_A/src/StudentDemo.java
@@ -0,0 +1,10 @@
+
+public class StudentDemo {
+	public static void main(String[] args) {
+		Student s1 = new Student();
+		//The field Student.rollno is not visible
+		s1.setValues(20,"Rahul", 9015088066L);
+		s1.display();
+		
+	}
+}
